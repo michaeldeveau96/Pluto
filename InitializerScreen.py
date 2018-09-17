c5f71cf76a1a363843b1734152cd7076e786693b
michaeldeveau96 <35409686+michaeldeveau96@users.noreply.github.com>
2018-09-17 12:34:17 -0700

    Update InitializerScreen.py

--- InitializerScreen.py
+++ InitializerScreen.py
@@ -1,5 +1,10 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
 
 
 class InitializerScreen(Screen):
+    
+    def startup(self):
+        username = TextInput(text='Enter router username: ')
+        password = TextInput(text='Enter router password: ')
     pass
